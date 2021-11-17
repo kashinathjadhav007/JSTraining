b5ec8e0b96f1e305eaca3b4f173235a448d55064
kashinathjadhav007 <kashinath.jadhav@successive.tech>
2021-11-17 07:46:07 +0530

    mytask

---
+++ Day6/assets/script.js
@@ -0,0 +1,44 @@
+// let user
+// {
+//     name:"kashi";
+//     age :23
+// }
+
+// for(const key in user)
+// {
+//     console.log(key);
+// }
+// console.log(user.name);
+// console.log(user[name]);
+
+let user ={name:"kashi" ,age:22};
+
+console.log(user)
+
+let clone={};
+
+for(let key in user)
+{
+    clone[key]=user[key];
+}
+
+console.log('cloned array',clone);
+console.log(Object.keys(user));
+console.log(Object.values(user))
+
+
+
+let val={
+result: function (age)
+    {
+        if(age<18)
+        {
+            console.log('age is minor')
+        }
+        else 
+        {
+            console.log('age is major')
+        }
+}
+}
+console.log(val.result(20));
